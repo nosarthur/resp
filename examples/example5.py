c5f14383f68e2fe0bf27db4fe32f515e24c68995
Shehan <parmar@gatech.edu>
2025-06-14 13:00:30 -0400

    reading RESP grid directly; using RESP charges instead of MPFIT; deleted create_difference_esp function

--- examples/example5.py
+++ examples/example5.py
@@ -1,57 +1,49 @@
 import os
 import psi4
 import resp
-from utils import charges_to_esp, compare_grid_esp, create_difference_esp
+from utils import charges_to_esp, compare_grid_esp
 
-mol = psi4.geometry(""" 
- N   6.27981210  -4.25303372   0.19313865
- N   6.06870777  -2.28125697   1.80544317
- H   3.66871156  -5.64628941  -1.96684001
- H   0.69034521  -2.37545694  -0.42864236
- H   3.35917442   0.17650601   3.09265017
- H   7.35266782  -1.99461311   2.90680702
- C   3.77952586  -1.21508453   1.76858172
- C   2.38242998  -2.55505538   0.04607681
- C   4.01771468  -4.38410490  -0.88761863
-units bohr
-""")
-
+mol = psi4.geometry(""" C   1.45051389  -0.06628932   0.00000000
+ H   1.75521613  -0.62865986  -0.87500146
+ H   1.75521613  -0.62865986   0.87500146
+ H   1.92173244   0.90485897   0.00000000
+ C  -0.04233122   0.09849378   0.00000000
+ O  -0.67064817  -1.07620915   0.00000000
+ H  -1.60837259  -0.91016601   0.00000000
+ O  -0.62675864   1.13160510   0.00000000""")
 mol.update_geometry()
 
 options = {'VDW_SCALE_FACTORS'  : [1.4, 1.6, 1.8, 2.0],
            'VDW_POINT_DENSITY'  : 1.0,
            'RESP_A'             : 0.0005,
            'RESP_B'             : 0.1,
-	   'METHOD_ESP'         : 'hf', 
-	   'BASIS_ESP'          : 'aug-cc-pvdz', 
            }
 
 # Call for first stage fit
 charges1 = resp.resp([mol], options)
-
 print('Electrostatic Potential Charges')
 print(charges1[0])
 print('Restrained Electrostatic Potential Charges')
 print(charges1[1])
 
-os.system("mv %i_%s_grid_esp.dat %i_%s_grid_esp_ESP.dat" %(1, mol.name(), 1, mol.name()))
+# Change the value of the RESP parameter A
+options['RESP_A'] = 0.001
 
-# MPFIT-generated charges (user-defined) 
-charges = [-0.39242, -0.02616, 0.10538, 0.13998, 0.12150, 0.25454, 0.00422, -0.30743, 0.10040]
+# Add constraint for atoms fixed in second stage fit
+constraint_charge = []
+for i in range(4, 8):
+    constraint_charge.append([charges1[1][i], [i+1]])
+options['constraint_charge'] = constraint_charge
+options['constraint_group'] = [[2, 3, 4]]
+options['grid'] = ['1_%s_grid.dat' %mol.name()]
+options['esp'] = ['1_%s_grid_esp.dat' %mol.name()]
 
-# Generate ESP grid files from charges
-charges_to_esp(mol, charges, options)
+# Call for second stage fit
+charges2 = resp.resp([mol], options)
 
-print(f"Generated grid files (for comparison charges):")
-print(f"  1_{mol.name()}_grid_esp.dat")
+# Get RESP charges
+print("\nStage Two:\n")
+print('RESP Charges')
+print(charges2[1])
 
-# Compare with RESP-generated ESP
-print("\nComparing MPFIT-generated ESP with RESP-generated ESP...")
-resp_esp = "1_default_grid_esp_ESP.dat"
-mpfit_esp = "1_default_grid_esp.dat"
-
-metrics = compare_grid_esp(resp_esp, mpfit_esp, verbose=True)
-
-# Create difference ESP file
-print("\nCreating difference ESP file...")
-create_difference_esp(resp_esp, mpfit_esp, output_file='difference_grid_esp.dat')
+charges_to_esp(mol, charges2[1], options, psi4_esp_file = "1_default_grid_esp.dat")
--- examples/utils.py
+++ examples/utils.py
@@ -12,7 +12,7 @@ from resp.vdw_surface import vdw_surface
 # Physical constants
 bohr_to_angstrom = 0.529177249
 
-def charges_to_esp(molecule, charges, options=None):
+def charges_to_esp(molecule, charges, options=None, psi4_esp_file="1_default_grid_esp.dat"):
     """Generate ESP grid files from given charges.
     
     Parameters
@@ -66,19 +66,6 @@ def charges_to_esp(molecule, charges, options=None):
         points.append(shell)
     points = np.concatenate(points)
     
-    # Handle grid filename
-    grid_filename = 'grid.dat'
-    if 'GRID' in options and options['GRID']:
-        grid_filename = options['GRID'][0]
-    
-    # Save grid file - same format as resp()
-    if 'Bohr' in str(molecule.units()):
-        points /= bohr_to_angstrom
-        np.savetxt(grid_filename, points, fmt='%15.10f')
-        points *= bohr_to_angstrom
-    else:
-        np.savetxt(grid_filename, points, fmt='%15.10f')
-    
     # Calculate ESP values - following same pattern as invr calculation in resp()
     esp_values = np.zeros(len(points))
     for i in range(len(points)):
@@ -87,24 +74,12 @@ def charges_to_esp(molecule, charges, options=None):
             # Convert to atomic units: ESP (a.u.) = charge * (1/r_angstrom) * bohr_to_angstrom
             esp_values[i] += charges[j] / distance * bohr_to_angstrom
     
-    # Handle esp filename
-    esp_filename = 'grid_esp.dat'
-    if 'ESP' in options and options['ESP']:
-        esp_filename = options['ESP'][0]
-    
-    # Save ESP file
-    np.savetxt(esp_filename, esp_values, fmt='%15.10f')
-    
-    # Move files to numbered format if default names were used
-    if grid_filename == 'grid.dat':
-        os.system("mv grid.dat 1_%s_grid.dat" % molecule.name())
-    if esp_filename == 'grid_esp.dat':
-        os.system("mv grid_esp.dat 1_%s_grid_esp.dat" % molecule.name())
-    
+    esp_psi4 = np.loadtxt(psi4_esp_file)
+    compare_grid_esp(esp_psi4, esp_values, verbose=True) 
     return
 
 
-def compare_grid_esp(file1, file2, verbose=True):
+def compare_grid_esp(esp1, esp2, verbose=True):
     """Compare two grid_esp.dat files and calculate metrics.
     
     Parameters
@@ -121,9 +96,6 @@ def compare_grid_esp(file1, file2, verbose=True):
     dict
         Dictionary containing comparison metrics
     """
-    # Load ESP values
-    esp1 = np.loadtxt(file1)
-    esp2 = np.loadtxt(file2)
     
     # Check if arrays have same shape
     if esp1.shape != esp2.shape:
@@ -138,9 +110,6 @@ def compare_grid_esp(file1, file2, verbose=True):
     
     # RMSE (L2 norm normalized by number of points)
     metrics['rmse'] = np.sqrt(np.mean(diff**2))
-    
-    # L1 norm (sum of absolute differences)
-    metrics['l1_norm'] = np.sum(np.abs(diff))
     
     # MAE (L1 norm normalized by number of points)
     metrics['mae'] = np.mean(np.abs(diff))
@@ -163,54 +132,9 @@ def compare_grid_esp(file1, file2, verbose=True):
         print(f"\nError Metrics:")
         print(f"  L2 norm:               {metrics['l2_norm']:.6e}")
         print(f"  RMSE:                  {metrics['rmse']:.6e}")
-        print(f"  L1 norm:               {metrics['l1_norm']:.6e}")
         print(f"  MAE:                   {metrics['mae']:.6e}")
         print(f"  Max absolute error:    {metrics['max_error']:.6e}")
         print(f"\nCorrelation Metrics:")
         print(f"  Pearson correlation:   {metrics['correlation']:.6f}")
         print(f"  R-squared:             {metrics['r_squared']:.6f}")
-        
-        # ESP value ranges
-        print(f"\nESP Value Ranges:")
-        print(f"  File 1 {file1}: [{np.min(esp1):.6e}, {np.max(esp1):.6e}]")
-        print(f"  File 2 {file2}: [{np.min(esp2):.6e}, {np.max(esp2):.6e}]")
-    
     return metrics
-
-
-def create_difference_esp(file1, file2, output_file='difference_grid_esp.dat'):
-    """Create a grid_esp.dat file containing the difference between two ESP files.
-    
-    Parameters
-    ----------
-    file1 : str
-        Path to first grid_esp.dat file
-    file2 : str
-        Path to second grid_esp.dat file
-    output_file : str
-        Path to output difference file (default: 'difference_grid_esp.dat')
-    
-    Returns
-    -------
-    np.ndarray
-        Array of difference values
-    """
-    # Load ESP values
-    esp1 = np.loadtxt(file1)
-    esp2 = np.loadtxt(file2)
-    
-    # Check if arrays have same shape
-    if esp1.shape != esp2.shape:
-        raise ValueError(f"ESP arrays have different shapes: {esp1.shape} vs {esp2.shape}")
-    
-    # Calculate difference
-    difference = esp1 - esp2
-    print(f"Difference between {file1} - {file2}")
-    
-    # Save difference to file
-    np.savetxt(output_file, difference, fmt='%14.10e')
-    
-    print(f"Created difference ESP file: {output_file}")
-    print(f"Difference range: [{np.min(difference):.6e}, {np.max(difference):.6e}]")
-    
-    return difference
